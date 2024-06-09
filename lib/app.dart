0dd6d3497fc45000b85a26eed630053989185266
Nijat Namazzade <nicat.namazzade@mail.ru>
2024-06-09 11:19:05 +0400

    feat: implement the fonts and assets with flutter_gen

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prosol_task/l10n/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:prosol_task/presentation/home/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,11 +17,13 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
+      debugShowCheckedModeBanner: false,
       locale: const Locale('en'),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const HomePage(),
     );
   }
 }

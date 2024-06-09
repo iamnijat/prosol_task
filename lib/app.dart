82cba8d87a29ecabc34c6c5203d4915c3eea2d04
Nijat Namazzade <nicat.namazzade@mail.ru>
2024-06-09 10:31:55 +0400

    feat: add localization support with intl package

---
+++ lib/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:prosol_task/l10n/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      supportedLocales: L10n.delegate.supportedLocales,
+      localizationsDelegates: const [
+        L10n.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: const Locale('en'),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
